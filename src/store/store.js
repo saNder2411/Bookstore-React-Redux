a77efd0d7abc629f9c4224c61b1cb7696c76624c
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-04-29 10:05:25 +0300

    Fix practice stringEnhancer.

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,27 @@
 import {createStore, compose} from 'redux';
 import reducer from './reducers/reducer';
 
+const logMiddleware = (store) => (dispatch) => (action) => {
+  console.log(action.type, store.getState());
+
+  return dispatch(action);
+}
+
+const stringMiddleware = (store) => (dispatch) => (action) => {
+
+  if (typeof action === `string`) {
+    return dispatch({type: action});
+  }
+
+  return dispatch(action);
+}
+
 const logEnhancer = (createStore) => (...args) => {
   const store = createStore(...args);
   const originalDispatch = store.dispatch;
   store.dispatch = (action) => {
     console.log(action.type);
+
     return originalDispatch(action);
   }
 
@@ -18,7 +34,7 @@ const stringEnhancer = (createStore) => (...args) => {
   store.dispatch = (action) => {
 
     if (typeof action === `string`) {
-      return {type: action};
+      return originalDispatch({type: action});
     }
 
     return originalDispatch(action);
