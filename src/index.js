7967ad6141913c14e359930b3b111c74cc110ca7
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-05-05 18:31:27 +0300

    clear code

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './components/app/app';
-import ErrorBoundary from './components/error-boundary/error-boundary';
-import BookstoreService from './services/bookstore-service';
-import {BookstoreServiceProvider} from './components/bookstore-service-context/bookstore-service-context';
-import store from './store/store';
+import App from "./components/app/app";
+import ErrorBoundary from "./components/error-boundary/error-boundary";
+import BookstoreService from "./services/bookstore-service";
+import { BookstoreServiceProvider } from "./components/bookstore-service-context/bookstore-service-context";
+import store from "./store/store";
 
-import * as serviceWorker from './serviceWorker';
-import './index.css';
-
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
 const bookstoreService = new BookstoreService();
 
 ReactDOM.render(
-  <Provider store={ store }>
+  <Provider store={store}>
     <ErrorBoundary>
-      <BookstoreServiceProvider value={ bookstoreService }>
+      <BookstoreServiceProvider value={bookstoreService}>
         <BrowserRouter>
-          <React.StrictMode>
-            <React.Suspense>
-              <App />
-            </React.Suspense>
-          </React.StrictMode>
+          <App />
         </BrowserRouter>
       </BookstoreServiceProvider>
     </ErrorBoundary>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
